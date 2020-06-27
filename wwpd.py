cd5f1a22f274045c76e404e8634e6e76ac17b7c0
syreal17 <ltjones@acm.org>
2020-06-27 16:14:28 -0400

    Add numberness checking script

---
+++ wwpd.py
@@ -0,0 +1,14 @@
+# What Would Python Determine?
+# does Python think the user input is a number?
+
+# Imports borrow code from elsewhere
+import math
+
+
+
+def is_number(n):
+	return not math.isnan(n)
+
+
+
+### BOOKMARK : loop, ask user for input, check is_number
