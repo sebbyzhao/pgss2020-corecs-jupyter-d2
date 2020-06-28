006d42870afba9ce1d273878f859befe7b29d1cd
syreal17 <ltjones@acm.org>
2020-06-28 20:33:14 +0000

    Add better comments and whitespace

--- verify.py
+++ verify.py
@@ -1,19 +1,40 @@
-# What Would Python Determine?
-# does Python think the user input is a number?
+# A handful of functions to support seeing what Python thinks is a number
+# AUTHOR: LT
+# EMAIL: See end of slides
 
-# _Imports_ borrow code from elsewhere
+
+
+# HINT: *import* borrows existing code from somewhere else
 import math
 
-# Global variables
+
+
+# Global variables... HINT: which means available anywhere in this file.
 thisIsntANumber = 17
+thisIsANumber = "psych! It's actually a string (of letters)"
+
+
 
 def is_number(n):
+    """Returns True if n is a number and false otherwise
+    
+    NOTE: This function is an inversion of the standard isnan (Is Not a Number)
+    function. Generally, I find asking questions in the positive 
+    more intuitive.
+    """
+    
 	return not math.isnan(n)
 
 
 
-### BOOKMARK : loop, ask user for input, check is_number
 def number_verification_service():
+    """Verify that user input is a number
+    
+    As a service, this function asks for user input and verifies until 
+    terminated
+    """
+
+    # NOTE: while ( True ) means that this loops "forever"
     while ( True ):
         user_entity = input("Enter something to verify if it is a number: ")
         
@@ -25,4 +46,5 @@ def number_verification_service():
 
 
 
+# Starts the number verification service
 number_verification_service()
